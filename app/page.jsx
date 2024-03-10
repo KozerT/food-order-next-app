b93998cf38edf0a0927231b74118aa836de4b363
KozerT <kozer.vizastaff@gmail.com>
2024-03-10 23:14:27 +0100

    style(home): enhance Home page component and add marketing text

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,47 @@
 import Link from "next/link";
+import classes from "./page.module.css";
 
 export default function Home() {
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>start</h1>
-      <p>
-        <Link href="/meals">Meals</Link>
-      </p>
-      <p>
-        <Link href="/meals/share">Share Meal</Link>
-      </p>
-      <p>
-        <Link href="/community">Community</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.slideshow}></div>
+        <div>
+          <div className={classes.hero}>
+            <h1>Foodie Fuel: Feed Your Obsession</h1>
+            <p>
+              Embark on a culinary quest. Explore dishes from every corner of
+              the earth.
+            </p>
+          </div>
+          <div className={classes.cta}>
+            <Link href="/community">Join the Foodies Community </Link>
+            <Link href="/meals"> Explore meals </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section className={classes.section}>
+          <h2>How it works</h2>
+          <p>
+            Welcome to the Global Kitchen: Your Gateway to Culinary Adventures!
+          </p>
+          <p>
+            NextLevel Food isn&apos;t a recipe book, it&apos;s a passport to a
+            world of flavors! Share your heritage dishes, discover exotic
+            cuisines, and connect with food lovers across the globe.
+          </p>
+        </section>
+        <section className={classes.section}>
+          <h2>Why NextLevel Food?</h2>
+          <p>Because Food is the Best Kind of Travel.</p>
+          <p>
+            Embark on a delicious journey with NextLevel Food. Explore new
+            cultures through their cuisine, share your own culinary traditions,
+            and connect with fellow food explorers.
+          </p>
+        </section>
+      </main>
+    </>
   );
 }
